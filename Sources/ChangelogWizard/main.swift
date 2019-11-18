709af44f1d3b327caa54ab4307f09367fc91ac98
Unknown <bszamody@gmail.com>
2019-11-19 08:55:02 +1100

    WIP

--- Sources/ChangelogWizard/main.swift
+++ Sources/ChangelogWizard/main.swift
@@ -1,8 +1,20 @@
+#!/usr/bin/swift
+
 import Foundation
+
+enum CustomError: Error {
+    case never
+    
+    var localizedDescription: String {
+        switch self {
+        case .never:
+            return "Never gonna happen"
+        }
+    }
+}
 
 class Main {
     let args: [String]
-    
     let fileManager: FileManager
     
     init(args: [String] = CommandLine.arguments, fileManager: FileManager = FileManager()) {
@@ -11,19 +23,22 @@ class Main {
     }
     
     func run() {
-        print(FileManager().currentDirectoryPath)
-//        shell(args)
+        do {
+            let output = try shell(args)
+            print(output)
+        } catch {
+            print(error)
+        }
     }
     
     @discardableResult
-    private func shell(_ commands: [String]) -> String {
+    private func shell(_ commands: [String]) throws -> String {
         let task = Process()
-        task.launchPath = "/bin/bash"
-//        task.arguments = commandsswift buid
-        
         let pipe = Pipe()
         task.standardOutput = pipe
-        task.launch()
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", commands.joined(separator: " ")]
+        try task.run()
         
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         return String(data: data, encoding: .utf8) ?? ""
